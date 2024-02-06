0c33c9de0549cd3ab8980ed58fc31226d2a84ae7
manuel perez <manuelperez.0000@gmail.com>
2024-02-06 17:51:54 -0400

    vercel integration

--- api/login.js
+++ api/login.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
         const response = await User.find()
         res.json({ message: 'success', status: true, body: response })
     } catch (error) {
-        res.json({ message: 'Ocurrio un error de comunicacion co la base de datos', status: false })
+        res.json({ message: 'Ocurrio un error de comunicacion con la base de datos', status: false })
     }
 })
 
