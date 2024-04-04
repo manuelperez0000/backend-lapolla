de8da67b339b246be00d7cba52ed7ff2de40108b
manuel perez <manuelperez.0000@gmail.com>
2024-04-04 14:28:31 -0400

    loginUserDataPayMethods

--- api/user/login.js
+++ api/user/login.js
@@ -6,7 +6,8 @@ const jwt = require('jsonwebtoken');
 const { DATA_TOKEN } = require('../../services/temporalEnv')
 const { findOneUsersWhitEmail, findOneUsersWhitEmailAndPassword } = require('../../db/controllers/userController')
 const responser = require('../../network/response')
-
+const { getConfig } = require('../../db/controllers/configController')
+const { getMethods } = require('../../db/controllers/methodController')
 router.post('/', cors(), async (req, res) => {
     try {
         const { email, password } = req.body
@@ -17,11 +18,23 @@ router.post('/', cors(), async (req, res) => {
 
         if (!userFinded) { throw "Usuario no registrado" }
 
-        const userData = await findOneUsersWhitEmailAndPassword(email, password)
+        const inicio = performance.now();
+
+        const _userData = findOneUsersWhitEmailAndPassword(email, password)
+        const _config = getConfig()
+        const _payMethods = getMethods()
+
+        const [userData, config, payMethods] = await Promise.all([_userData, _config, _payMethods])
+
+        const fin = performance.now();
+
+        const tiempo_transcurrido = fin - inicio;
+
+        console.log(`Tiempo de ejecución: ${tiempo_transcurrido.toFixed(6)} milisegundos`);
 
         if (!userData) { throw "Usuario o contraseña incorrecta" }
 
-        const body = jwt.sign({ userData }, DATA_TOKEN, { expiresIn: '100d' })
+        const body = jwt.sign({ userData, config, payMethods }, DATA_TOKEN, { expiresIn: '100d' })
         responser.success({ res, message: "Success", body })
 
     } catch (error) { responser.error({ res, message: error }) }
