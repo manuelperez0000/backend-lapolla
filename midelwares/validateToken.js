106419debdc7aa6bdbe697cb9d4686083fb8ab2f
manuel perez <manuelperez.0000@gmail.com>
2024-04-18 21:34:46 -0400

    quit testing bearer

--- midelwares/validateToken.js
+++ midelwares/validateToken.js
@@ -4,7 +4,6 @@ const responser = require('../network/response')
 const validateToken = (req, res, next) => {
     const authorization = req.headers.authorization || ''
     const token = authorization.slice(7)
-    console.log("Token:", token)
     try {
         jwt.verify(token, DATA_TOKEN, (err, decoded) => {
             if (err) {
