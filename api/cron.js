67295d5045f46da7cbe36bd627231a014a6b52f6
manuel perez <manuelperez.0000@gmail.com>
2024-07-10 15:18:14 -0400

    vercel cron prueba

--- api/cron.js
+++ api/cron.js
@@ -1,24 +1,5 @@
-async function handler(request, response) {
-    const data = {
-        "name": "Pollabot",
-        "email": "pollabot@gmail.com",
-        "ci": "12345678",
-        "password": "123456",
-        "phone": "04141234567",
-        "repassword": "123456"
-    }
-    const url = "https://backend-lapolla.vercel.app/api/v1/register"
-    const apiRes = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    })
-    const jsonApiRes = await apiRes.json()
-    console.log(jsonApiRes)
-   
-    return response.json({ jsonApiRes });
+function handler(req, res) {
+    res.status(200).end('Hello Cron!');
   }
 
   module.exports = handler
