9d9786615fd9f1e281fe665f94f43c6060b87384
manuel perez <manuelperez.0000@gmail.com>
2024-08-10 13:54:33 -0400

    robbi deleted

--- api/tickets/getTicketDate.js
+++ api/tickets/getTicketDate.js
@@ -2,12 +2,12 @@
 const express = require('express')
 const router = express.Router()
 const responser = require('../../network/response')
-const { required } = require('../../services/validate')
+/* const { required } = require('../../services/validate')
 const { getTicketDate } = require('../../db/controllers/ticketController')
-const validateToken = require('../../midelwares/validateToken')
+const validateToken = require('../../midelwares/validateToken') */
 
-router.post('/', validateToken, async (req, res) => {
-    try {
+router.post('/', async (req, res) => {
+    /* try {
         const user = res.user.user
         const data = req.body
         required([data.usuario, data.date])
@@ -18,13 +18,14 @@ router.post('/', validateToken, async (req, res) => {
         const result = await getTicketDate(obj, user)
         
         responser.success({ res, message: "success", body: result })
-    } catch (error) {
-        console.log(error)
-        responser.error({ res, message: error })
-    }
+        } catch (error) {
+            console.log(error)
+            responser.error({ res, message: error })
+            } */
+   responser.success({ res, message: "success", body:{} })
 })
 
-router.get('/', (req,res) =>{
+router.get('/', async (req,res) =>{
     responser.success({ res, message: "success getting", body:{} })
 })
 
