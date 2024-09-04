6781dd0864c0ee918394e60e9b70fcd5d6e11c5d
manuel perez <manuelperez.0000@gmail.com>
2024-09-04 15:18:40 -0400

    prueba db 2

--- db/conection.js
+++ db/conection.js
@@ -1,17 +1,12 @@
 /* const runScripts = require('./mongooseScripts') */
 const mongoose = require('mongoose')
-
 const environment = require('../services/temporalEnv')
 const DB_URI = environment.DB_URI
 
-//const DB_URI = "mongodb+srv://bitmarketperu:Asking03-.@cluster0.7dmn5.mongodb.net/quinielas?retryWrites=true&w=majority&appName=quinielas"
-
 const dbConnect = async () => {
-    
         await mongoose.connect(DB_URI)
         console.log('DB connected')
         //runScripts()
-    
 }
 
 module.exports = dbConnect;
